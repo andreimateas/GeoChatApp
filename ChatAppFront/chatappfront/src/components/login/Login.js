756cd26abdda0f841a418300d9e0483a99781aea
Andrei Mateaș <mateasandrei10@gmail.com>
2023-04-19 01:13:46 +0300

    v1.8

---
+++ ChatAppFront/chatappfront/src/components/login/Login.js
@@ -0,0 +1,9 @@
+
+export default function Login(){
+    return (
+        <div id={"login"}>
+            <button id={"buttonLogin"}>
+                Login
+            </button>
+        </div>);
+}
