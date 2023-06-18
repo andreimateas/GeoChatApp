906c17d1ed07775d795c6a8873ff7ae3fce08ae6
Andrei Mateaș <mateasandrei10@gmail.com>
2023-06-18 20:41:58 +0300

    v1.18

--- ChatAppFront/chatappfront/src/components/home/Home.js
+++ ChatAppFront/chatappfront/src/components/home/Home.js
@@ -8,14 +8,14 @@ export default function Home(){
     return (
         <div className="home-page">
             <div className="home-page__content">
-                <h1 className="home-page__heading">Welcome to ChatApp!</h1>
+                <h1 className="home-page__heading">Welcome to GeoChat App!</h1>
 
                 <div className="home-page__button">
-                    <Link to="/login" className="home-page__link">Login</Link>
+                    <Link to="/login" className="home-page__link_login">Login</Link>
                 </div>
                 <p className="home-page__text">Don't have an account yet?</p>
                 <div className="home-page__button">
-                    <Link to="/register" className="home-page__link">Register</Link>
+                    <Link to="/register" className="home-page__link_register">Register</Link>
                 </div>
             </div>
         </div>
