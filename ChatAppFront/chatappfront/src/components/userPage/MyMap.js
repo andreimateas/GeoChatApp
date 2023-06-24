9269140d975192462826ae14c20b1ab818e59b1d
Andrei Mateaș <mateasandrei10@gmail.com>
2023-06-25 00:40:35 +0300

    v1.29

--- ChatAppFront/chatappfront/src/components/userPage/MyMap.js
+++ ChatAppFront/chatappfront/src/components/userPage/MyMap.js
@@ -1,56 +1,107 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
 import './MyMap.css';
+import romaniaStates from './ro_judete_poligon.geojson';
+import MapboxGlSupported from 'mapbox-gl-supported';
 
 const MyMap = () => {
 
+    const mapContainerRef = useRef(null);
+
     mapboxgl.accessToken = 'pk.eyJ1IjoiZm9vemVuYmVyZyIsImEiOiJjbGphZHY3YjMxbjI0M2Rxem4ybHF1ZWc5In0.e5wmrFTSG5JBOz84CCgvsw';
+
+    const [selectedState, setSelectedState] = useState(null);
 
     const [viewport, setViewport] = useState({
         width: '100%',
         height: 400,
-        latitude: 0, // Initial latitude
-        longitude: 0, // Initial longitude
-        zoom: 10, // Initial zoom level
+        latitude: 0,
+        longitude: 0,
+        zoom: 10,
     });
 
-
-
     useEffect(() => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiZm9vemVuYmVyZyIsImEiOiJjbGphZHY3YjMxbjI0M2Rxem4ybHF1ZWc5In0.e5wmrFTSG5JBOz84CCgvsw';
-        const bounds = [
-            [20.2611, 43.6187], // Southwest coordinates [longitude, latitude]
-            [29.7497, 48.2656], // Northeast coordinates [longitude, latitude]
-        ];
+        const isMapboxSupported = MapboxGlSupported();
 
+        if (isMapboxSupported) {
+            mapboxgl.accessToken = 'pk.eyJ1IjoiZm9vemVuYmVyZyIsImEiOiJjbGphZHY3YjMxbjI0M2Rxem4ybHF1ZWc5In0.e5wmrFTSG5JBOz84CCgvsw';
 
-        const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11', // Replace with your desired map style
-            bounds: bounds, // Set the initial boundaries to focus on Romania
-            maxBounds: bounds, // Restrict panning and zooming outside of Romania
-            minZoom: 5, // Set the minimum allowed zoom level
-            attributionControl: false, // Hide the Mapbox logo and attribution
-        });
+            const bounds = [
+                [20.2611, 43.6187],
+                [29.7497, 48.2656],
+            ];
 
-        return () => {
-            map.remove(); // Clean up the map when the component unmounts
-        };
-    }, []);
+            const map = new mapboxgl.Map({
+                container: mapContainerRef.current,
+                style: 'mapbox://styles/mapbox/streets-v11',
+                bounds: bounds,
+                maxBounds: bounds,
+                minZoom: 5,
+            });
+
+            map.on('load', () => {
+                map.addSource('romania-states', {
+                    type: 'geojson',
+                    data: romaniaStates,
+                });
+
+                map.addLayer({
+                    id: 'romania-states-fill',
+                    type: 'fill',
+                    source: 'romania-states',
+                    layout: {},
+                    paint: {
+                        'fill-color': [
+                            'case',
+                            ['==', ['get', 'name'], selectedState],
+                            '#FF0000',
+                            'transparent',
+                        ],
+                        'fill-opacity': 0.5,
+                    },
+                });
+
+                map.on('click', 'romania-states-fill', (e) => {
+                    const clickedState = e.features[0].properties.name;
+
+                    if (selectedState === clickedState) {
+
+                        setSelectedState(null);
+                    } else {
+                        setSelectedState(clickedState);
+                    }
+                });
+
+                map.on('mousemove', 'romania-states-fill', () => {
+                    map.getCanvas().style.cursor = 'pointer';
+                });
+
+                map.on('mouseleave', 'romania-states-fill', () => {
+                    map.getCanvas().style.cursor = '';
+                });
+            });
+
+            return () => {
+                map.remove();
+            };
+        }
+    }, [selectedState]);
 
 
     return (
 
 
-        <ReactMapGL
-            {...viewport}
-            onViewportChange={setViewport}
-            mapboxApiAccessToken="pk.eyJ1IjoiZm9vemVuYmVyZyIsImEiOiJjbGphZHY3YjMxbjI0M2Rxem4ybHF1ZWc5In0.e5wmrFTSG5JBOz84CCgvsw"
-        >
-            {/* Add any additional components or overlays */}
-        </ReactMapGL>
+        <div
+            style={{
+                width: '90%',
+                margin: '0 auto',
+                height: '500px',
+            }}
+            id="map-container"
+            ref={mapContainerRef}
+        />
 
 
     );
