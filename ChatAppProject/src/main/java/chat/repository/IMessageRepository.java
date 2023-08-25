2b1a38d6b7f0155f9e0e7e9b2d7237aacfe2651a
Andrei Mateaș <mateasandrei10@gmail.com>
2023-08-25 19:39:40 +0300

    v1.57

--- ChatAppProject/src/main/java/chat/repository/IMessageRepository.java
+++ ChatAppProject/src/main/java/chat/repository/IMessageRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface IMessageRepository extends JpaRepository<Message, Integer> {
 
-    @Query("SELECT msg FROM Message msg WHERE (msg.sender= :user1 AND msg.receiver= :user2) OR (msg.sender= :user2 AND msg.receiver= :user1)")
+    @Query("SELECT msg FROM Message msg WHERE (msg.sender= :user1 AND msg.receiver= :user2) OR (msg.sender= :user2 AND msg.receiver= :user1) ORDER BY msg.date ASC")
     public List<Message> getMessagesByUsers(@Param("user1") String user1, @Param("user2") String user2);
 }
